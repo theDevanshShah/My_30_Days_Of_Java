8ac1ebd4b1bd1af6b7bb9336e20105a5b781c4ad
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-05-15 17:35:42 +0530

    Day 1

---
+++ src/Main_Package/HackerRank_Solved_Problems/RuffWork.java
@@ -0,0 +1,7 @@
+package Main_Package.HackerRank_Solved_Problems;
+
+public class RuffWork {
+    public static void main(String[] args){
+        System.out.println("Hello I'm Ruff File");
+    }
+}
