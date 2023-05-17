31d58655cb7fc8d1a7460cee5c4fc0b2dec7fb29
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-05-17 23:07:12 +0530

    Day 3

--- src/Main_Package/HackerRank_Solved_Problems/RuffWork.java
+++ src/Main_Package/HackerRank_Solved_Problems/RuffWork.java
@@ -3,96 +3,26 @@ package Main_Package.HackerRank_Solved_Problems;
 import java.util.Scanner;
 
 public class RuffWork {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        double mainPrice = sc.nextDouble();
-        int tipPercent = sc.nextInt();
-        int taxPercent = sc.nextInt();
-        double actualTip = (mainPrice * tipPercent) / 100 ;
-        System.out.println(actualTip);
-        double actualTax = (mainPrice * taxPercent)/ 100;
-        System.out.println(actualTax);
-        double total = mainPrice + actualTip + actualTax + 0.50;
-        System.out.println((int)total);
-
-    }
-}
-
-
-/* This is the perfect way to write this code
-Here, Google Bard used Math.round() method to print amount as nearest integer
-
-import java.util.*;
-
-public class RuffWork {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double mainPrice = sc.nextDouble();
-        int tipPercent = sc.nextInt();
-        int taxPercent = sc.nextInt();
-
-        double tip = mainPrice * tipPercent / 100;
-        double tax = mainPrice * taxPercent / 100;
-
-        double total = mainPrice + tip + tax;
-        int roundedTotal = (int) Math.round(total);
-
-        System.out.println(roundedTotal);
+        int a = sc.nextInt();
+        if (a % 2 != 0) {
+        System.out.println("Weird");
+        }
+        else if (a == 2 && a == 4){
+            System.out.println("Not Weird");
+        }
+        else if (a == 6 && a == 8 && a == 10 && a == 12 && a == 14 && a == 16 && a == 18){
+            System.out.println("Weird");
+        }
+        else {
+            System.out.println("Not Weird");
+        }
     }
 }
-*/
 
 
-
-
-/* DAY 2
-Objective
-In this challenge, you will work with arithmetic operators. Check out the Tutorial tab for learning materials and an instructional video.
-
-Task
-Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
-
-Example
-
-
-
-A tip of 15% * 100 = 15, and the taxes are 8% * 100 = 8. Print the value  and return from the function.
-
-Function Description
-Complete the solve function in the editor below.
-
-solve has the following parameters:
-
-int meal_cost: the cost of food before tip and tax
-int tip_percent: the tip percentage
-int tax_percent: the tax percentage
-Returns The function returns nothing. Print the calculated value, rounded to the nearest integer.
-
-Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result.
-
-Input Format
-
-There are  lines of numeric input:
-The first line has a double,  (the cost of the meal before tax and tip).
-The second line has an integer,  (the percentage of  being added as tip).
-The third line has an integer,  (the percentage of  being added as tax).
-
-Sample Input
-
-12.00
-20
-8
-Sample Output
-
-15
-Explanation
-
-Given:
-, ,
-
-Calculations:
-
-
-
-
-We round  to the nearest integer and print the result, .*/
+        //If  n is odd, print Weird --
+        //If  n is even and in the inclusive range of  2 to 5 , print Not Weird
+        //If  n is even and in the inclusive range of 6 to 20 , print Weird
+        //If  n is even and greater than 20 , print Not Weird
