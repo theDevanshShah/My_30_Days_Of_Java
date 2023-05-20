8faee78732d7037cc008b9beec2a80fb779b14c6
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-05-20 15:33:29 +0530

    Day 6

--- src/Main_Package/HackerRank_Solved_Problems/RuffWork.java
+++ src/Main_Package/HackerRank_Solved_Problems/RuffWork.java
@@ -3,26 +3,39 @@ package Main_Package.HackerRank_Solved_Problems;
 import java.util.Scanner;
 
 public class RuffWork {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        if (a % 2 != 0) {
-        System.out.println("Weird");
+        int n = sc.nextInt();
+
+
+
+
+
+        for( int m = 0; m < n ; m++){
+
+            String str = sc.next();
+            char[] charArray = str.toCharArray();
+
+            for(int i = 0; i < charArray.length;i+=2){
+                System.out.print(charArray[i]);
+            }
+            System.out.print(" ");
+            for(int j = 1; j < charArray.length;j+=2){
+                System.out.print(charArray[j]);
+
+            }
+            System.out.println("");
+
+
+
+
         }
-        else if (a == 2 && a == 4){
-            System.out.println("Not Weird");
-        }
-        else if (a == 6 && a == 8 && a == 10 && a == 12 && a == 14 && a == 16 && a == 18){
-            System.out.println("Weird");
-        }
-        else {
-            System.out.println("Not Weird");
-        }
+
+
+
+
+
+
     }
 }
-
-
-        //If  n is odd, print Weird --
-        //If  n is even and in the inclusive range of  2 to 5 , print Not Weird
-        //If  n is even and in the inclusive range of 6 to 20 , print Weird
-        //If  n is even and greater than 20 , print Not Weird
