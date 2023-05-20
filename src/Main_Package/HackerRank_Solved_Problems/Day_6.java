b0bb42d3db167189fd4b0086dfe90849a7b595d7
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-05-20 15:35:27 +0530

    Day 6

---
+++ src/Main_Package/HackerRank_Solved_Problems/Day_6.java
@@ -0,0 +1,41 @@
+// Day_6 String, Loops , Conditional , Operators
+package Main_Package.HackerRank_Solved_Problems;
+import java.util.Scanner;
+
+public class Day_6 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+
+
+
+        for( int m = 0; m < n ; m++){
+
+            String str = sc.next();
+            char[] charArray = str.toCharArray();
+
+            for(int i = 0; i < charArray.length;i+=2){
+                System.out.print(charArray[i]);
+            }
+            System.out.print(" ");
+            for(int j = 1; j < charArray.length;j+=2){
+                System.out.print(charArray[j]);
+
+            }
+            System.out.println("");
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
